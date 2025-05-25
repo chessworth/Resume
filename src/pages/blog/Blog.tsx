b7a0c44a44633c69f244b0f12dd7ae200916a67a
Gursimran Singh <56034211+chessworth@users.noreply.github.com>
2025-05-25 11:02:09 -0400

    Added local blog saving

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,14 +1,27 @@
 import './Blog.css';
 import BlogListing from './BlogListing/BlogListing';
+import blogs from '../../data/blogs.json';
+import { useSearchParams } from 'react-router-dom';
 
-function Blog() {
+const Blog: React.FC = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = searchParams.get("id");
+    if (id !== null) {
+        const blog = blogs.find((blog) => blog.id === Number(id));
+        return (
+            <div className='pageContainer'>
+                <h1 className='title'>{blog?.name}</h1>   
+                <div dangerouslySetInnerHTML={{ __html: blog?.body || '' }} />
+            </div>
+        )
+    }
     return (
         <div className='pageContainer'>
             <h1 className='title'>Blog</h1>   
             <ul className='blogList'>
-                <li><BlogListing linkUrl="/blog" title="Blog 1" description="Description for Blog 1" /></li>
-                <li><BlogListing linkUrl="/blog" title="Blog 2" description="Description for Blog 2" /></li>
-                <li><BlogListing linkUrl="/blog" title="Blog 3" description="Description for Blog 3" /></li>
+                {blogs.map((blog) => (
+                    <li key={blog.id}><BlogListing linkUrl={"/blog?id=" + blog.id} title={blog.name} description={blog.description} /></li>
+                ))}
             </ul>
         </div>
     )
