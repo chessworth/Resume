c9012c11ef5ecbd02f88dca13ce5becb2a482ad9
Gursimran Singh <56034211+chessworth@users.noreply.github.com>
2025-05-25 11:18:23 -0400

    Blog listing styling fix and removed dummy blogs

--- src/pages/blog/BlogListing/BlogListing.tsx
+++ src/pages/blog/BlogListing/BlogListing.tsx
@@ -6,7 +6,7 @@ function BlogListing({linkUrl, title, description} : {linkUrl : string, title : 
     return (
         <div>
             <h1 className='listingTitle'><a href={linkUrl}>{title}</a></h1>
-            <p className='listingDescription'>{description}</p>
+            <p className='listingDescription'>{description.length > 150 ? description.substring(0, 150) + '...' : description}</p>
         </div>
     )
 }
