3514e74904d9f5d452f7e28a3409b8459052c454
Gursimran Singh <56034211+chessworth@users.noreply.github.com>
2025-05-27 00:12:48 -0400

    change to navlink

--- src/pages/blog/BlogListing/BlogListing.tsx
+++ src/pages/blog/BlogListing/BlogListing.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './BlogListing.css';
 
 function BlogListing({linkUrl, title, description} : {linkUrl : string, title : string, description : string}) {
@@ -5,7 +6,7 @@ function BlogListing({linkUrl, title, description} : {linkUrl : string, title : 
 
     return (
         <div>
-            <h1 className='listingTitle'><a href={linkUrl}>{title}</a></h1>
+            <h1 className='listingTitle'><NavLink to={linkUrl}>{title}</NavLink></h1>
             <p className='listingDescription'>{description.length > 150 ? description.substring(0, 150) + '...' : description}</p>
         </div>
     )
